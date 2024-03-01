c89ca7477e121ce91a5f3f2794306d26fab718a8
AndreyKizimenko <andreykizimenko@gmail.com>
2024-02-29 23:52:28 -0600

    fetching users from DB and populating the dropdown

--- app/issues/[issueID]/AssigneeDropdown.tsx
+++ app/issues/[issueID]/AssigneeDropdown.tsx
@@ -1,16 +1,31 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssigneeDropdown = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <>
       <Select.Root defaultValue="unassigned">
         <Select.Trigger />
         <Select.Content position="popper">
           <Select.Item value="unassigned">Unassigned</Select.Item>
-          <Select.Item value="apple">User A</Select.Item>
-          <Select.Item value="orange">User B</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Content>
       </Select.Root>
     </>
