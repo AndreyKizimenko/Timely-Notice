91488640cf1a8d265db9ccabddf070cb68459ddf
AndreyKizimenko <andreykizimenko@gmail.com>
2024-03-08 13:38:03 -0600

    built an issue summary component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import LatestIssues from './LatestIssues'
+import React from "react";
+import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <LatestIssues />
-  )
-}
+    <>
+      <LatestIssues />
+      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+    </>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
