d8b837959fbc30bfc3d893625d2a48e0288774dc
AndreyKizimenko <andreykizimenko@gmail.com>
2024-04-07 21:52:00 -0500

    Added negative test cases to the new issue form suite

--- app/components/FormError.tsx
+++ app/components/FormError.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const FormError = ({ children }: PropsWithChildren) => {
-  return <p className="mt-1 text-xs text-right text-red-400" role="alert">{children}</p>;
+  return <p data-cy="input-error-message" className="mt-1 text-xs text-right text-red-400" role="alert">{children}</p>;
 };
 
 export default FormError;
--- cypress/e2e/newIssue.cy.ts
+++ cypress/e2e/newIssue.cy.ts
@@ -1,9 +1,13 @@
+import { faker } from "@faker-js/faker";
+
 describe("New Issue", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/issues/list");
     cy.signIn({ skipAuth: true });
     cy.getByData("new-issue-button").click();
   });
+
+  // Positive scenarios
 
   it("URL and page elements", () => {
     cy.location("pathname").should("eq", "/issues/new");
@@ -23,5 +27,36 @@ describe("New Issue", () => {
 
     cy.getByData("t-header-createdAt").click();
     cy.get("tbody tr td").eq(1).should("contain", currentDateTime);
+  });
+
+  // Negative scenarios
+
+  it("Submitting with various fields empty", () => {
+    // All fields empty
+    cy.getByData("new-issue-submit").click();
+    cy.getByData("input-error-message").eq(0).should("contain", "at least 1 character");
+    cy.getByData("input-error-message").eq(1).should("contain", "at least 5 characters");
+    // Title empty
+    cy.getByData("new-issue-description").clear().type(faker.string.alphanumeric(5));
+    cy.getByData("new-issue-submit").click();
+    cy.getByData("input-error-message").should("contain", "at least 1 character");
+    // Description empty
+    cy.getByData("new-issue-title").clear().type(faker.string.alphanumeric(2));
+    cy.getByData("new-issue-description").clear()
+    cy.getByData("new-issue-submit").click();
+    cy.getByData("input-error-message").should("contain", "at least 5 characters");
+  });
+
+  it("Form schema verifications", () => {
+    // Title too long
+    cy.getByData("new-issue-title").clear().type(faker.string.alphanumeric(256));
+    cy.getByData("new-issue-description").clear().type(faker.string.alphanumeric(5));
+    cy.getByData("new-issue-submit").click();
+    cy.getByData("input-error-message").should("contain", "longer than 255 characters");
+    // Description too short
+    cy.getByData("new-issue-title").clear().type(faker.string.alphanumeric(2));
+    cy.getByData("new-issue-description").clear().type(faker.string.alphanumeric(4));
+    cy.getByData("new-issue-submit").click();
+    cy.getByData("input-error-message").should("contain", "at least 5 characters");
   });
 });
